a194c715056900ca332f611090e78db02ea3c8ea
falconwoo <falconwoo@126.com>
2017-08-02 02:01:08 +0800

    refactor Postnet transferZipcodeToBarcode, implement truly transfer

--- src/main/java/Postnet.java
+++ src/main/java/Postnet.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
 public class Postnet {
 
     public static String encoding(String zipcode) {
@@ -10,15 +14,68 @@ public class Postnet {
     }
 
     private static String transferZipcodeToBarcode(char[] zipCodeItems) {
-        String barcode = "";
-        if(String.valueOf(zipCodeItems).equals("95713")){
-            barcode = "| |:|:: :|:|: |:::| :::|| ::||: :|:|: |";
+
+        List<Integer> zipCodeList = new ArrayList<Integer>();
+
+        for(char zipCodeItem : zipCodeItems){
+            if(zipCodeItem == '-'){
+                continue;
+            }
+            zipCodeList.add(Character.getNumericValue(zipCodeItem));
         }
 
-        if(String.valueOf(zipCodeItems).equals("55555-1237")){
-            barcode = "| :|:|: :|:|: :|:|: :|:|: :|:|: :::|| ::|:| ::||: |:::| ::|:| |";
+        int checkDigital = calcCheckDigital(zipCodeList);
+
+        zipCodeList.add(checkDigital);
+
+        StringBuffer barcodeBuffer = new StringBuffer("| ");
+        for(Integer zipCodeItem : zipCodeList){
+            switch(zipCodeItem){
+                case 0:
+                    barcodeBuffer.append("||:::").append(" ");
+                    break;
+                case 1:
+                    barcodeBuffer.append(":::||").append(" ");
+                    break;
+                case 2:
+                    barcodeBuffer.append("::|:|").append(" ");
+                    break;
+                case 3:
+                    barcodeBuffer.append("::||:").append(" ");
+                    break;
+                case 4:
+                    barcodeBuffer.append(":|::|").append(" ");
+                    break;
+                case 5:
+                    barcodeBuffer.append(":|:|:").append(" ");
+                    break;
+                case 6:
+                    barcodeBuffer.append(":||::").append(" ");
+                    break;
+                case 7:
+                    barcodeBuffer.append("|:::|").append(" ");
+                    break;
+                case 8:
+                    barcodeBuffer.append("|::|:").append(" ");
+                    break;
+                case 9:
+                    barcodeBuffer.append("|:|::").append(" ");
+                    break;
+            }
         }
-        return barcode;
+
+        barcodeBuffer.append("|");
+
+        return barcodeBuffer.toString();
+    }
+
+    private static int calcCheckDigital(List<Integer>  zipCodeList) {
+        int total = 0;
+        for(int zipCodeItem : zipCodeList){
+            total += zipCodeItem;
+        }
+        int checkDigital = 10 - total % 10;
+        return checkDigital;
     }
 
     private static char[] splitZipcode(String zipcode) {
