0d23efac30938967a08fd0c3ecb10eca7779dbb9
falconwoo <falconwoo@126.com>
2017-08-02 01:16:55 +0800

    refactor Postnet encoding, extract 2 subfunction: splitZipcode and transferZipcodeToBarcode

--- src/main/java/Postnet.java
+++ src/main/java/Postnet.java
@@ -1,14 +1,28 @@
 public class Postnet {
 
     public static String encoding(String zipcode) {
+
+        char[] zipCodeItems = splitZipcode(zipcode);
+
+        String barcode = transferZipcodeToBarcode(zipCodeItems);
+
+        return barcode;
+    }
+
+    private static String transferZipcodeToBarcode(char[] zipCodeItems) {
         String barcode = "";
-        if(zipcode.equals("95713")){
+        if(String.valueOf(zipCodeItems).equals("95713")){
             barcode = "| |:|:: :|:|: |:::| :::|| ::||: :|:|: |";
         }
-        if(zipcode.equals("55555-1237")){
+
+        if(String.valueOf(zipCodeItems).equals("55555-1237")){
             barcode = "| :|:|: :|:|: :|:|: :|:|: :|:|: :::|| ::|:| ::||: |:::| ::|:| |";
         }
         return barcode;
+    }
+
+    private static char[] splitZipcode(String zipcode) {
+        return zipcode.toCharArray();
     }
 
     public static String decoding(String barcode) {
